fa955efe7bbf62210ade3d53e445e48b1db8b45b
Sergey <aLozhka@yahoo.com>
2022-06-10 00:04:10 +0300

    working on
    merging two files

--- RGR/RGR2/FilesHandler.pas
+++ RGR/RGR2/FilesHandler.pas
@@ -18,19 +18,45 @@ BEGIN {PrintTree}
       WRITE(FOut, Ptr^.Word);
       GetTreeIntoFile(FOut, Ptr^.RLink)
     END
-END; {PrintTree}
+END; {PrintTree}   
 
 
 PROCEDURE MergeSortFiles(VAR FOut, F1, F2: WordsFile);
 VAR
   Word1, Word2: WordHandle;
 BEGIN {MergeSortFiles}
+
   IF NOT EOF(F1)
   THEN
     READ(F1, Word1);
   IF NOT EOF(F2)
   THEN
     READ(F2, Word2);
+    
+  IF (EOF(F1)) AND (EOF(F2))
+  THEN
+    BEGIN
+      IF Word1.Value < Word2.Value
+      THEN
+        BEGIN
+          WRITE(FOut, Word1);
+          READ(F1, Word1)
+        END;
+      IF Word1.Value > Word2.Value
+      THEN
+        BEGIN
+          WRITE(FOut, Word2);
+          READ(F2, Word2)
+        END;
+      IF Word1.Value = Word2.Value
+      THEN
+        BEGIN
+          WRITE(FOut, Word1);
+          READ(F1, Word1);
+          READ(F2, Word2)
+        END
+    END;
+    
   WHILE (NOT EOF(F1)) AND (NOT EOF(F2))
   DO
     BEGIN
@@ -52,7 +78,15 @@ BEGIN {MergeSortFiles}
           WRITE(FOut, Word1);
           READ(F1, Word1);
           READ(F2, Word2)
-        END; 
+        END
+    END;
+  WHILE NOT EOF(F1)
+  DO
+    BEGIN
+      IF Word1 < Word2
+      THEN
+        BEGIN
+          WRITE(//глянь Merge процедуру в какой-то из лаб
     END
 END; {MergeSortFiles}
 
--- RGR/RGR2/RGR2.pas
+++ RGR/RGR2/RGR2.pas
@@ -1,14 +1,14 @@
 PROGRAM CountWords(INPUT, OUTPUT);
 USES
-  TextHandler;
+  TextHandler,
+  FilesHandler;
 VAR
   WordsTree: Tree;
+
 
 BEGIN {CountWords}
   WordsTree := NIL;
   WordsTree := TextHandle(INPUT);
   PrintTree(OUTPUT, WordsTree);
-  WRITELN('¬сего ', CWords, ' слов, из них уникальных: ', CUniqueWords)
+  WRITELN('¬сего ', CWords, ' слов, из них уникальных: ', CUniqueWords);
 END. {CountWords}
-//рекомендую почитать формулировку 
-//задани€ rgr и полезный комментарий
